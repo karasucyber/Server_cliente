621fa5fc62e87976a0cf45be3b6aa5db0e03ec5c
aquillae7 <aquillae777@gmail.com>
2023-09-15 08:02:13 -0300

    config

---
+++ src/test/java/com/app/produtos/sql_produtos/SqlProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.produtos.sql_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
