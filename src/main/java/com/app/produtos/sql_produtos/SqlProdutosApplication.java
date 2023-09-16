7781d4f2e5ef60ac71c14c774e6cf13b7d54f310
aquillae7 <aquillae777@gmail.com>
2023-09-16 09:22:32 -0300

    erro

---
+++ src/main/java/com/app/produtos/sql_produtos/SqlProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.app.produtos.sql_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlProdutosApplication.class, args);
+	}
+
+}
